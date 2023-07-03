e98699089fd43c08330275d35ab568d9cdb5b5c4
Edmondo Pentangelo <edmondo@gmail.com>
2023-07-03 22:46:41 +0100

    fix scrolling

--- src/components/TabPanel.tsx
+++ src/components/TabPanel.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Typography, makeStyles } from '@material-ui/core';
 
 interface TabPanelProps {
   children: React.ReactNode;
@@ -7,15 +7,28 @@ interface TabPanelProps {
   index: number;
 }
 
+const useStyles = makeStyles((theme) => ({
+  scrollableContent: {
+    maxHeight: 'calc(100vh - 64px)', // or the height you prefer
+    overflowY: 'auto', // enables vertical scrolling
+  },
+}));
+
+
 const TabPanel: React.FC<TabPanelProps> = ({ children, value, index }) => {
+  const classes = useStyles();
   return (
     <div
       role="tabpanel"
       hidden={value !== index}
+      id={`wrapped-tabpanel-${index}`}
+      aria-labelledby={`wrapped-tab-${index}`}
     >
       {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
+        <Box p={0}>
+          <Typography>
+            <div className={classes.scrollableContent}>{children}</div>
+          </Typography>
         </Box>
       )}
     </div>
